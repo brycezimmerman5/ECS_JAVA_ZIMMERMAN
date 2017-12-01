98fa1ac5ec62cf85b6470fa19a375f614b85ca9f
Bryce Zimmerman <32139895+brycezimmerman5@users.noreply.github.com>
2017-12-01 10:23:41 -0800

    StillUsingVariables and YourSchedule

--- VARIABLES/src/StillUsingVariables.java
+++ VARIABLES/src/StillUsingVariables.java
@@ -1,5 +1,16 @@
 public class StillUsingVariables 
 {
 	public static void main( String[] args)
+	{
+	int mygradyear;
+	String myname;
+	
+	myname = "Bryce Zimmerman";
+	mygradyear = 2021;
+	
+	System.out.println("My name is "+myname+" and I'll graduate in "+mygradyear);
+	
+	
+	}
 
 }
