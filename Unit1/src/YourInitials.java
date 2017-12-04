06f654f98b2899198f86e9784b4deb9e67a1e622
Bryce Zimmerman <32139895+brycezimmerman5@users.noreply.github.com>
2017-12-04 10:44:57 -0800

    Organized folders

---
+++ Unit1/src/YourInitials.java
@@ -0,0 +1,18 @@
+
+public class YourInitials
+{
+	public static void main(String[] args)
+
+	{
+		System.out.println("Bryce Charles Zimmerman");
+		System.out.println("BBBB   CCC   ZZZZZ"); 
+		System.out.println("B   B C   C     Z");
+		System.out.println("B   B C        Z");
+		System.out.println("BBBB  C       Z");
+		System.out.println("B   B C      Z");
+		System.out.println("B   B C   C Z");
+		System.out.println("BBBB   CCC  ZZZZZ");
+	}
+
+
+}
---
+++ Unit1/src/firstprog.java
@@ -0,0 +1,13 @@
+//Comment for my FirstProg
+//Just prints out a message to the display
+public class firstprog 
+{
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Ting goes");
+		
+		
+	}
+
+}
